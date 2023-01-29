ffb6a1d4b7498f1d805c7aa5cc8089399a09ad0e
StoicInsight <gettantolcen@gmail.com>
2023-01-28 20:18:48 -0500

    search input

--- dictionary/src/App.js
+++ dictionary/src/App.js
@@ -2,12 +2,31 @@ import Navigation from './components/Navigation/Navigation';
 import Display from './components/Display/Display';
 import SearchInput from './components/SearchInput/SeachInput'
 import './App.css'
+import { useState, useEffect } from 'react'
+
 
 function App() {
+
+  const [ searchInput, setSearchInput ] = useState('')
+  const [ display, setDisplay ] = useState([])
+
+  const updateSearch = (e) => {
+    setSearchInput(e.target.value)
+  }
+
+  const searchDictionary = (e) => {
+    e.preventDefault()
+
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchInput}`)
+      .then(response => response.json())
+      .then(data => console.log(data[0]))
+
+  }   
+  
   return (
     <div className="App">
       <Navigation/>
-      <SearchInput/>
+      <SearchInput search={updateSearch} submit={searchDictionary}/>
       <Display/>
     </div>
   );
--- dictionary/src/components/SearchInput/SeachInput.jsx
+++ dictionary/src/components/SearchInput/SeachInput.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import './search-input.css'
 import { ReactComponent as SearchIcon } from '../../assets/images/icon-search.svg'
 
-const SeachInput = () => {
+const SeachInput = ({ search, submit }) => {
   return (
     <div className='search-input'>
-      <form action="submit">
+      <form action="submit" onSubmit={submit}>
         <input 
         type="text" 
         placeholder='Search dictionary...'
+        onChange={search}
         />
         <button type='submit'> <img src='../../assets/images/icon-search.svg' alt="" /> </button>
       </form>
-              <img src='../../assets/images/icon-search.svg' alt="" className='img'/>
 
     </div>
   )
