406ea4536196d8f9e35c3010821f6e7a6b9c7ee3
StoicInsight <gettantolcen@gmail.com>
2023-02-09 23:50:09 -0500

    tried adding audio

--- dictionary/src/components/Display/Display.jsx
+++ dictionary/src/components/Display/Display.jsx
@@ -2,19 +2,20 @@ import React from 'react'
 import './Display.css'
 
 const Display = ({display}) => {
-  console.log(display[0].word)
+  console.log(display[0].phonetics[2].audio)
   if( display.length > 0  ) {
     return (
       <div>
-        { display.map((item) => {
+        {/* { display.map((item) => {
           // console.log(item.length)
           return (
             <>
               <h1>{item.word}</h1>
             </>
           )
-        })}
-      </div>
+        })} */}
+        <h1>No Word? {display[0].word}</h1>
+      </div>  
     )
   } else {
     return (
